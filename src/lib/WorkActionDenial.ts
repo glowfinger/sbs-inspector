95d90cf8b7078d65fedfc276de211a66eee7f9a6
Nate <nathan-dp@hotmail.co.uk>
2023-03-30 15:09:30 +0100

    diasbled links unless temperatures are set, making components

--- src/lib/WorkActionDenial.ts
+++ src/lib/WorkActionDenial.ts
@@ -1,5 +1,31 @@
-export default function workActionDenial(){
-    if(results.length === 0){
+import type WorkResult from "./types/WorkResult";
 
+export let button;
+export let results: WorkResult;
+export default function workActionDenial() {
+  let finalCheck = 
+  setTemperature(results);
+  setFailsafe(results);
+
+  return finalCheck;
+}
+
+function setTemperature(results) {
+  results.addEventListener("boolean", () => {
+    if (results.temperature === 0) {
+      button.disabled = true;
+    } else {
+      button.disabled = false;
     }
+  });
+}
+
+function setFailsafe(results) {
+  results.addEventListener("boolean", () => {
+    if (results.value === null) {
+      button.disabled = true;
+    } else {
+      button.disabled = false;
+    }
+  });
 }
